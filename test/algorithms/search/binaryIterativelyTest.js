58532a81da97e30e9f0c56d21d0cbd0d006ce311
Seba Belmar <si.belmar@gmail.com>
2017-11-23 01:06:22 -0700

    Arranged syntaxt.

--- test/algorithms/search/binaryIterativelyTest.js
+++ test/algorithms/search/binaryIterativelyTest.js
@@ -2,21 +2,24 @@ expect = require('../../test-helper').expect;
 const {bSearchIteratively} = require('../../../src/algorithms/search/binaryIteratively');
 
 describe('Binary Search Iteratively', ()=>{
-  var sortedArray = [1,2,3,4,5,6,7,8,9]
-  var endIdx = sortedArray.length - 1
+  var sortedArray = [1,2,3,4,5,6,7,8,9];
+  var endIdx = sortedArray.length - 1;
 
   it('returns index of target when located at the middle', ()=>{
-    expect(bSearchIteratively(sortedArray, 5)).to.equal(4)
-  })
+    expect(bSearchIteratively(sortedArray, 5)).to.equal(4);
+  });
+
   it('returns index of target number when located above and bellow middle', ()=>{
-    expect(bSearchIteratively(sortedArray, 2)).to.equal(1)
-    expect(bSearchIteratively(sortedArray, 7)).to.equal(6)
-  })
+    expect(bSearchIteratively(sortedArray, 2)).to.equal(1);
+    expect(bSearchIteratively(sortedArray, 7)).to.equal(6);
+  });
+
   it('returns index of target number when located on the extrems', ()=>{
-    expect(bSearchIteratively(sortedArray, 1)).to.equal(0)
-    expect(bSearchIteratively(sortedArray, 9)).to.equal(8)
-  })
+    expect(bSearchIteratively(sortedArray, 1)).to.equal(0);
+    expect(bSearchIteratively(sortedArray, 9)).to.equal(8);
+  });
+
   it('returns false if target number not present', ()=>{
-    expect(bSearchIteratively(sortedArray, 11)).to.be.false
-  })
-})
+    expect(bSearchIteratively(sortedArray, 11)).to.equal(false);
+  });
+});
--- test/algorithms/sorting/selectionTest.js
+++ test/algorithms/sorting/selectionTest.js
@@ -1,10 +1,10 @@
 const {expect} = require('../../test-helper');
 const {selectionSort} = require('../../../src/algorithms/sorting/selection');
 
-describe('Selection Sort', function(){
+describe('Selection Sort', ()=>{
   let sampleArray = [2, 3, 6, 8, 0, 5];
 
-  it('sorts array of n elements', function(){
+  it('sorts array of n elements', ()=>{
     expect(selectionSort(sampleArray).equals([0, 2, 3, 5, 6, 8])).to.equal(true);
   });
 });
