6705c6821d0b89f4e523e58778b3546a20b539b1
Seba Belmar <si.belmar@gmail.com>
2017-11-24 15:53:25 -0700

    Changed name to lookforlowest element index.

--- test/ds/array/lookForLowestElementIndexTest.js
+++ test/ds/array/lookForLowestElementIndexTest.js
@@ -4,15 +4,15 @@ const {findLowestIndex} = require('../../../src/ds/array/lookForLowestElementInd
 describe('Finds lowest element index in sub array given an starting index', ()=>{
   let sampleArray = [4, 2, 6, 1, 8, 6, 9, 10];
 
-  it('returns lower when starting from index = 0', ()=>{
+  it('returns lowest when starting from index = 0', ()=>{
     expect(findLowestIndex(sampleArray, 0)).to.equal(3);
   });
 
-  it('returns lower when starting from index = length-1', ()=>{
+  it('returns lowest when starting from index = length-1', ()=>{
     expect(findLowestIndex(sampleArray, 7)).to.equal(7);
   });
 
-  it('returns lower when starting from  0 < index < length-1', ()=>{
+  it('returns lowest when starting from  0 < index < length-1', ()=>{
     expect(findLowestIndex(sampleArray, 2)).to.equal(3);
     expect(findLowestIndex(sampleArray, 4)).to.equal(5);
   });
