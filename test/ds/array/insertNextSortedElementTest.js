a1df82e495bca04830fc25830f1ca0e20b612494
Seba Belmar <si.belmar@gmail.com>
2017-11-24 23:16:05 -0700

    Refactored insert test.

--- test/ds/array/insertNextSortedElementTest.js
+++ test/ds/array/insertNextSortedElementTest.js
@@ -1,17 +1,17 @@
 const {expect} = require('../../test-helper');
 const {insert} = require('../../../src/ds/array/insertNextSortedElement');
 
-describe('Insert element in asc order', function(){
+describe('Insert element in asc order', ()=>{
   var sampleArray;
-  beforeEach(function(){
+  beforeEach(()=>{
     sampleArray = [7,5,3,1,4,6];
   });
 
-  it('inserts element in order when 0 < index < length - 1', function(){
+  it('inserts element in order when 0 < index < length - 1', ()=>{
     expect(insert(sampleArray, 2, 1).equals([1,7,5,3,4,6])).to.equal(true);
   });
 
-  it('inserts element in order when index of second element', function(){
+  it('inserts element in order when index of second element', ()=>{
     expect(insert(sampleArray, 0, 5).equals([5,7,3,1,4,6])).to.equal(true);
   });
 });
