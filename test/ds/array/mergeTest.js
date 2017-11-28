7dbfae35fb2a5212ec84fa89d28dd7b55066001c
Seba Belmar <si.belmar@gmail.com>
2017-11-27 21:37:20 -0700

    Fixed typo on power.

--- test/ds/array/mergeTest.js
+++ test/ds/array/mergeTest.js
@@ -6,9 +6,9 @@ const {merge} = require('../../../src/ds/array/merge');
 describe('Merge 2 subarrays semi-sorted into the passed original array', () => {
   let sampleArray = [1,4,6,8,9,2,5,7,12,13];
   let middle = Math.floor((0 + (sampleArray.length-1)) / 2);
-  let sorted = merge(sampleArray, 0, middle, (sampleArray.length-1));
+  merge(sampleArray, 0, middle, (sampleArray.length-1));
 
-  it('sorts the array when halfs are ascending', function(){
+  it('sorts the array when halfs are ascending', () => {
     expect(sampleArray.equals([ 1, 2, 4, 5, 6, 7, 8, 9, 12, 13 ])).to.equal(true);
   });
 });
