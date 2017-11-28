5092ad06a41342f758e4c95a536881101e585473
Seba Belmar <si.belmar@gmail.com>
2017-11-28 14:04:51 -0700

    Removed console log.

--- test/algorithms/sorting/mergeTest.js
+++ test/algorithms/sorting/mergeTest.js
@@ -5,8 +5,6 @@ describe('Selection Sort', ()=>{
   let sampleArray = [2, 3, 6, 8, 0, 5];
   mergeSort(sampleArray, 0, sampleArray.length-1);
 
-  console.log(sampleArray)
-
   it('sorts array of n elements', ()=>{
     expect(sampleArray.equals([0, 2, 3, 5, 6, 8])).to.equal(true);
   });
