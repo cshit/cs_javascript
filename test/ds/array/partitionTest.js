0d15b9943ffd3f9789505c00299596e93df042ac
Seba Belmar <si.belmar@gmail.com>
2017-11-30 12:55:50 -0700

    Refactored partition test.

--- test/ds/array/partitionTest.js
+++ test/ds/array/partitionTest.js
@@ -4,9 +4,10 @@ const {partition} = require('../../../src/ds/array/partition');
 
 describe('Partition Array', () => {
   let sampleArray = [5,3,4,1,6,0,2];
+  let pivotIndex  = partition(sampleArray, 0, sampleArray.length-1);
 
-  it('returns pivot new index', () => {
-    expect(partition(sampleArray, 0, sampleArray.length-1)).to.equal(2);
+  it('returns pivots new index', () => {
+    expect(pivotIndex).to.equal(2);
   });
 
   it('divides the original array into lower < pivot < higher elements', () => {
