dcbce6b4d7f624c8c7f1e88908043c6b59858585
Seba Belmar <si.belmar@gmail.com>
2017-12-05 10:20:57 -0700

    Implemented and tested singly liked list.

---
+++ src/ds/linkedList/singly.js
@@ -0,0 +1,64 @@
+class SinglyLinkedList{
+
+  constructor(value){
+    this.next = null;
+    this.value = value;
+  }
+
+  // Iterativily
+  add(value){
+    let currentNode = this;
+    while(currentNode.next !== null) {
+      currentNode = this.next;
+    }
+
+    currentNode.next = new SinglyLinkedList(value);
+    return currentNode.next;
+  }
+
+  // Recursive
+  find(value){
+    if(this.value === value){
+      return this;
+    }
+
+    if(this.next == null){
+      return null;
+    }
+
+    return this.next.find(value);
+  }
+
+  delete(value){
+    // find sucessor
+    // sucessor next = current.context
+    // deallocate memory
+
+    const sucessor = this._findSucessor(value);
+    // console.log(sucessor.next);
+    const current = sucessor.next;
+    sucessor.next = current.next;
+  }
+
+  // Recursive
+  traverse(){
+    console.log(this.value);
+    if(this.next === null){
+      return;
+    }
+    this.next.traverse();
+  }
+
+  // Recursive
+  _findSucessor(value, sucessor){
+    if(this.value === value){
+      return sucessor;
+    }else if (this.next !== null) {
+      return this.next._findSucessor(value, this);
+    }
+
+    return null;
+  }
+}
+
+exports.SinglyLinkedList = SinglyLinkedList;
---
+++ test/ds/linkedList/singlyTest.js
@@ -0,0 +1,75 @@
+const {expect} = require('../../test-helper');
+const {SinglyLinkedList} = require('../../../src/ds/linkedList/singly');
+
+
+describe('Singly Linked List', function(){
+  let head;
+
+  beforeEach(function(){
+    head = new SinglyLinkedList('value');
+  });
+
+  context('Creates', function(){
+    it('creates a list with head, next = nil', function(){
+      expect(head.next).to.equal(null);
+    });
+
+    it('creates a list with head, value = value', function(){
+      expect(head.value).to.equal('value');
+    });
+  });
+
+
+  context('Add', function(){
+    it('adds a node to the head with some value', function(){
+      const value2 = 'value2';
+      head.add(value2);
+
+      expect(head.next.value).to.equal(value2);
+    });
+  });
+
+
+  context('Find', function(){
+    it('finds a node with a specific value place at the end', function(){
+      head.add('value2').add('value3').add('value4');
+      const target = 'value4';
+
+      expect(head.find(target)).to.be.an.instanceof(SinglyLinkedList);
+    });
+
+    it('finds a node with a specific value place in the middle', function(){
+      head.add('value2').add('value3').add('value4');
+      const target = 'value3';
+
+      expect(head.find(target)).to.be.an.instanceof(SinglyLinkedList);
+    });
+
+    it('returns null when a value not found', function(){
+      head.add('value2').add('value3').add('value4');
+      const target = 'nothere';
+
+      expect(head.find(target)).to.be.a('null');
+    });
+  });
+
+  context('Delete', function(){
+
+    it('deletes a node placed in as the middle', function(){
+      head.add('value2').add('value3').add('value4');
+      head.delete('value3');
+      head.traverse();
+
+      expect(head.find('value3')).to.be.a('null');
+    });
+
+    it('deletes a node placed in at the end', function(){
+      head.add('value2').add('value3').add('value4');
+      head.delete('value4');
+      head.traverse();
+
+      expect(head.find('value4')).to.be.a('null');
+    });
+  });
+
+});
